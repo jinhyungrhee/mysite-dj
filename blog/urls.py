ffb495725882e28141fe4c985327dd88bf09061c
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-07-25 12:14:19 +0900

    CBV로 블로그 포스트 목록 페이지 제작

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.PostList.as_view()), # CBV 포스트 목록 페이지
+    # path('', views.index), # FBV 포스트 목록 페이지
     path('<int:pk>/', views.single_post_page),
-    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
-    # DB에 query 날리기
-    posts = Post.objects.all().order_by('-pk') # 최신 포스트부터 보여주기
+# CBV 포스트 목록 페이지
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts, # template으로 보내는 데이터(context)
-        }
-    )
+# FBV 포스트 목록 페이지
+# def index(request):
+#     # DB에 query 날리기
+#     posts = Post.objects.all().order_by('-pk') # 최신 포스트부터 보여주기
+#
+#     return render(
+#         request,
+#         'index.html',
+#         {
+#             'posts': posts, # template으로 보내는 데이터(context)
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
