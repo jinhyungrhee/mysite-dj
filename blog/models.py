cebfff65786379356da91b7e788f7015cd6b12b5
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-07-27 15:28:57 +0900

    포스트 목록 페이지와 상세 페이지에 태그 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -30,6 +40,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True ,on_delete=models.SET_NULL)
+
+    tags = models.ManyToManyField(Tag, blank=True)
 
     # 외부에 나타나는 이름/제목(admin Post 목록 포함)
     def __str__(self):
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class TestView(TestCase):
     def setUp(self):
@@ -12,6 +12,10 @@ class TestView(TestCase):
         # 카테고리 데이터 생성
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
+        # 테그 데이터 생성
+        self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
+        self.tag_python = Tag.objects.create(name='python', slug='python')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
         # 포스트 데이터 생성
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
@@ -19,6 +23,8 @@ class TestView(TestCase):
             category=self.category_programming, # 카테고리 추가
             author=self.user_trump,  
         )
+        self.post_001.tags.add(self.tag_hello) # 태그 연결(ManyToManyField)
+
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
@@ -30,6 +36,8 @@ class TestView(TestCase):
             content='category가 없을 수도 있죠.',
             author=self.user_obama
         )
+        self.post_003.tags.add(self.tag_python_kor) # 태그 연결(ManyToManyField)
+        self.post_003.tags.add(self.tag_python)
 
 
     # 네비게이션 바 점검 -> 모듈 테스트를 위한 함수X(네이밍 컨벤션)
@@ -79,17 +87,32 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title, post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.post_001.author.username.upper(), post_001_card.text)
+        # 태그 테스트
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+        # 태그 테스트
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_python.name, post_002_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_002_card.text)
 
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn('미분류', post_003_card.text)
         self.assertIn(self.post_003.title, post_003_card.text)
+        self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+        # 태그 테스트
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_python.name, post_003_card.text)
+        self.assertIn(self.tag_python_kor.name, post_003_card.text)
 
-        self.assertIn(self.user_trump.username.upper(), main_area.text)
-        self.assertIn(self.user_obama.username.upper(), main_area.text)
+        # self.assertIn(self.user_trump.username.upper(), main_area.text)
+        # self.assertIn(self.user_obama.username.upper(), main_area.text)
 
         # 포스트가 없는 경우
         Post.objects.all().delete()
@@ -120,6 +143,10 @@ class TestView(TestCase):
 
         self.assertIn(self.user_trump.username.upper(), main_area.text)
         self.assertIn(self.post_001.content, post_area.text)
+        # 상세 페이지 태그 테스트(post_001에 대해서)
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
 
     # 카테고리 페이지 테스트 함수
     def test_category_page(self):
